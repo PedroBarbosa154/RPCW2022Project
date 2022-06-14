568a50552a26988722a1cbe8d3fd7ceb283e4a98
joaoprp111 <joaoprp111@gmail.com>
2022-06-14 17:01:29 +0100

    API pronta, forms, a começar a validação do zip e a autenticação

--- api-server/controllers/recursos.js
+++ api-server/controllers/recursos.js
@@ -1,7 +1,42 @@
 var Recurso = require('../models/recursos')
+var mongoose = require('mongoose')
 
 //Inserir Recurso (metadados)
 module.exports.inserir = metadados =>{
     var novoRecurso = new Recurso(metadados)
     return novoRecurso.save()
+}
+
+//Atualizar um recurso
+module.exports.atualizar = (rid, tipo) => {
+    return Recurso
+        .findOneAndUpdate({_id:mongoose.Types.ObjectId(rid)},{tipo: tipo},{new:true})
+}
+
+//Listar os recursos
+module.exports.listar = () => {
+    return Recurso
+        .find()
+        .exec()
+}
+
+//Listar os recursos por tipo
+module.exports.listarPorTipo = tipo => {
+    return Recurso
+        .find({tipo: tipo})
+        .exec()
+}
+
+//Listar os recursos que contenham uma palavra no titulo
+module.exports.listarPorTipo = pal => {
+    return Recurso
+        .find({titulo: {$regex: pal}})
+        .exec()
+}
+
+//Listar um recurso com id igual a rid
+module.exports.listarPorRid = rid => {
+    return Recurso
+        .findOne({_id:mongoose.Types.ObjectId(rid)})
+        .exec()
 }
--- api-server/routes/api.js
+++ api-server/routes/api.js
@@ -1,16 +1,59 @@
 var express = require('express');
 var router = express.Router();
 var Recurso = require('../controllers/recursos')
+var url = require('url')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET recurso por rid. */
+router.get('/recursos/:rid', function(req, res, next) {
+  var rid = req.params.rid
+  Recurso.listarPorRid(rid)
+    .then(dados => res.status(200).jsonp(dados))
+    .catch(e => res.status(502).jsonp({error: e}))
 });
 
+/* GET recursos. */
+router.get('/recursos', function(req, res, next) {
+  var q = url.parse(req.url,true).query
+  if (q.tipo != undefined){
+    var tipo = q.tipo
+    Recurso.listarPorTipo(tipo)
+      .then(dados => {
+        console.log('Resposta: ' + dados)
+        res.status(200).jsonp(dados)
+      })
+      .catch(e => res.status(503).jsonp({error: e}))
+  } else if(q.q != undefined){
+    var pal = q.q
+    Recurso.listarComPalavra(pal)
+      .then(dados => {
+        console.log('Resposta: ' + dados)
+        res.status(200).jsonp(dados)
+      })
+      .catch(e => res.status(504).jsonp({error: e}))
+  }
+  else {
+    Recurso.listar()
+      .then(dados =>{
+        console.log('Resposta: ' + dados)
+        res.status(200).jsonp(dados)
+      })
+      .catch(e => res.status(500).jsonp({error: e}))
+  }
+});
+
+/* POST de um recurso. */
 router.post('/recursos', function(req, res, next) {
   Recurso.inserir(req.body)
-    .then(dados => res.status(201).jsonp({dados: dados}))
-    .catch(e => res.status(500).jsonp({error: e}))
+    .then(dados => res.status(201).jsonp(dados))
+    .catch(e => res.status(501).jsonp({error: e}))
+});
+
+/* PUT de um recurso. */
+router.put('/recursos/:rid', function(req, res, next) {
+  var rid = req.params.rid
+  Recurso.atualizar(rid, req.body.tipo)
+    .then(dados => res.status(201).jsonp(dados))
+    .catch(e => res.status(505).jsonp({error: e}))
 });
 
 module.exports = router;
