9f8fd72a883c5d8bdb6a0257597a8555013d7361
joaoprp111 <joaoprp111@gmail.com>
2022-06-16 15:11:15 +0100

    api-server com mais proteção

--- api-server/routes/api.js
+++ api-server/routes/api.js
@@ -2,6 +2,20 @@ var express = require('express');
 var router = express.Router();
 var Recurso = require('../controllers/recursos')
 var url = require('url')
+var jwt = require('jsonwebtoken');
+
+//É melhor verificar o token em todas as rotas que precisam de login por causa dos acessos vindos do Postman
+function verificaToken(req, res, next){
+  var myToken = req.query.token || req.body.token;
+  console.log(myToken)
+  jwt.verify(myToken, 'ProjetoRPCW2022', function(e, payload){
+    if(e) res.status(401).jsonp({error: 'Erro na verificação do token: ' + e})
+    else {
+      console.log("Token verificado e válido")
+      next()
+    } 
+  })
+}
 
 function verificaNivel(autorizados,req,res,next){
   if(autorizados.includes(req.nivel))
@@ -11,7 +25,7 @@ function verificaNivel(autorizados,req,res,next){
 }
 
 /* GET recurso por rid. */
-router.get('/recursos/:rid', function(req, res, next) {
+router.get('/recursos/:rid', verificaToken, function(req, res, next) {
   var rid = req.params.rid
   Recurso.listarPorRid(rid)
     .then(dados => res.status(200).jsonp(dados))
@@ -19,7 +33,7 @@ router.get('/recursos/:rid', function(req, res, next) {
 });
 
 /* GET recursos. */
-router.get('/recursos', function(req, res, next) {
+router.get('/recursos', verificaToken, function(req, res, next) {
   var q = url.parse(req.url,true).query
   if (q.tipo != undefined){
     var tipo = q.tipo
@@ -49,14 +63,14 @@ router.get('/recursos', function(req, res, next) {
 });
 
 /* POST de um recurso. */
-router.post('/recursos', function(req,res,next){verificaNivel(["admin","produtor"],req,res,next)}, function(req, res) {
+router.post('/recursos', verificaToken, function(req,res,next){verificaNivel(["admin","produtor"],req,res,next)}, function(req, res) {
   Recurso.inserir(req.body)
     .then(dados => res.status(201).jsonp(dados))
     .catch(e => {console.log(e);res.status(501).jsonp({error: e})})
 });
 
 /* PUT de um recurso. */
-router.put('/recursos/:rid', function(req,res,next){verificaNivel(["admin","produtor"],req,res,next)}, function(req, res) {
+router.put('/recursos/:rid', verificaToken, function(req,res,next){verificaNivel(["admin","produtor"],req,res,next)}, function(req, res) {
   var rid = req.params.rid
   Recurso.atualizar(rid, req.body.tipo)
     .then(dados => res.status(201).jsonp(dados))
